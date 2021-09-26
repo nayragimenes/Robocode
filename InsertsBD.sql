d4497555896a88f0e4a17a1daba5ee8d4ede1033
Nayra Gimenes Danni <nayragimenes@gmail.com>
2021-09-26 15:31:26 -0300

    inserido alguns inserts

---
+++ InsertsBD.sql
@@ -0,0 +1,33 @@
+INSERT INTO cidades VALUES (1,'Criciúma');
+INSERT INTO cidades VALUES (2,'Nova Veneza');
+
+
+INSERT INTO bairros VALUES (1,'Santa Bárbara');
+INSERT INTO bairros VALUES (2,'Centro');
+
+
+INSERT INTO logradouros VALUES (1,'Lucia Milioli', 'R');
+INSERT INTO logradouros VALUES (2,'Dos Imigrantes', 'R');
+
+
+INSERT INTO racas VALUES (1,'SRD');
+INSERT INTO racas VALUES (2,'Pastor Alemão');
+INSERT INTO racas VALUES (3,'Bulldog Francês');
+INSERT INTO racas VALUES (4,'Pinscher Miniatura');
+INSERT INTO racas VALUES (5,'Poodle');
+
+
+INSERT INTO pets VALUES (1, 1, 'Bob', NULL, NULL, NULL, NULL, NULL, 'N', NULL, NULL);
+INSERT INTO pets VALUES (2, 1, 'Lola', NULL, NULL, NULL, NULL, NULL, 'S', NULL, NULL);
+INSERT INTO pets VALUES (3, 1, 'Amy', NULL, NULL, NULL, NULL, NULL, 'N', NULL, NULL);
+INSERT INTO pets VALUES (4, 1, 'Toby', NULL, NULL, NULL, NULL, NULL, 'N', NULL, NULL);
+INSERT INTO pets VALUES (5, 1, 'Thor', NULL, NULL, NULL, NULL, NULL, 'N', NULL, NULL);
+
+
+INSERT INTO pessoas VALUES (1,	'Maria', '5612600', '78782389012', 13091983, '88802020', 516, 'casa', 1, 1, 1);
+INSERT INTO pessoas VALUES (2,	'João', '6789011', '41539328090', 02031995, '88865000', 463, 'apt 3', 2, 2, 2);
+
+
+INSERT INTO adocoes VALUES (1, 1, 2, 'se compromteu com a castracao', '20-09-2021', 50);
+INSERT INTO adocoes VALUES (2, 2, 5, 'se comprometeu com a vacinação', '13-08-2020', 10);
+
